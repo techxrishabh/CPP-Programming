134aa7432c06364166345a586cdceb53fa5e0d7a
techxrishabh <rishabh22204901@gmail.com>
2024-02-11 11:38:14 +0530

    added C++ variables

---
+++ 6. C++ VARIABLES/1. Declare-variable.cpp
@@ -0,0 +1,10 @@
+// Variables are containers for storing data values.
+
+// In C++, there are different types of variables (defined with different keywords), for example:
+
+// a. int - stores integers (whole numbers), without decimals, such as 123 or -123
+// b. double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+// c. char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+// d. string - stores text, such as "Hello World". String values are surrounded by double quotes
+// e. bool - stores values with two states: true or false
+
---
+++ 6. C++ VARIABLES/2. Constants.cpp
@@ -0,0 +1,12 @@
+// Constants
+
+// When we do not want others (or yourself) to change existing variable values, use the const keyword (this will declare the variable as "constant", which means unchangeable and read-only):
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     const int var1 = 10; // var1 = 10;
+//     var1 = 20; // will give an error because we use const (this will declare the variable as "constant", which means unchangeable and read-only)
+//     cout << var1;
+// }
