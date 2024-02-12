7b5ba52b1a5df33d865460fd1e32fc9729a44f3b
techxrishabh <rishabh22204901@gmail.com>
2024-02-12 17:49:59 +0530

    added C++ Operators

---
+++ 9 C++ OPERATORS/1. Arithmetic.cpp
@@ -0,0 +1,20 @@
+// Operators are used to perform operations on variables and values.\
+
+// Arithmetic Operator - Arithmetic operators are used to perform common mathematical operations.
+
+
+// Operator	                    Name	                Description	                    Example
+
+//     +	                  Addition	           Adds together two values	             x + y	
+
+//     -	                 Subtraction	   Subtracts one value from another	         x - y	
+
+//     *	                Multiplication	         Multiplies two values	             x * y	
+
+//     /	                   Division     	Divides one value by another	         x / y	
+
+//     %	                   Modulus	        Returns the division remainder	         x % y	
+
+//     ++	                  Increment	        Increases the value of a variable by 1	  ++x	
+
+//     --	                  Decrement	        Decreases the value of a variable by 1	  --x
---
+++ 9 C++ OPERATORS/2. Assignment.cpp
@@ -0,0 +1,26 @@
+// A list of all assignment operators:
+
+
+// Operator	                                Example	                                Same As
+
+//     =	                                 x = 5	                                x = 5
+
+//     +=	                                 x += 3	                                x = x + 3
+
+//     -=	                                 x -= 3	                                x = x - 3
+
+//     *=	                                 x *= 3	                                x = x * 3
+
+//     /=	                                 x /= 3	                                x = x / 3	
+
+//     %=	                                 x %= 3	                                x = x % 3	
+
+//     &=	                                 x &= 3	                                x = x & 3	
+
+//     |=	                                 x |= 3	                                x = x | 3	
+
+//     ^=	                                 x ^= 3	                                x = x ^ 3	
+
+//     >>=	                                 x >>= 3	                            x = x >> 3	
+
+//     <<=	                                 x <<= 3	                            x = x << 3
