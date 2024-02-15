1423d891618389e1b82693c35d04926e21fe83f3
techxrishabh <rishabh22204901@gmail.com>
2024-02-15 18:39:03 +0530

    added C++ Maths

---
+++ 11. C++ MATH/math.cpp
@@ -0,0 +1,28 @@
+// C++ Math
+// C++ has many functions that allows you to perform mathematical tasks on numbers.
+
+
+// Max and min
+
+// The max(x,y) function can be used to find the highest value of x and y:
+// The min(x,y) function can be used to find the lowest value of x and y:
+
+
+// C++ <cmath> - Header
+// Other functions, such as sqrt (square root), round (rounds a number) and log (natural logarithm), can be found in the <cmath> header file:
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+    cout << sqrt(64) << "\n";
+    cout << round(2.7) << "\n";
+    cout << log(2) << "\n";
+    cout << max(2, 4) << "\n";
+    cout << min(2, 4) << "\n";
+}
+
+
+// go here ==== https://www.w3schools.com/cpp/cpp_math.asp
+// If you want to learn more about these maths functions
