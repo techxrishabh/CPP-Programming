8f13c5ba3eed5cbbec38c5847cbb2216b89fa215
techxrishabh <rishabh22204901@gmail.com>
2024-02-17 17:52:02 +0530

    added switch

---
+++ 14. C++ SWITCH/switch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int day;
+    cout << "Enter the no. of day : \n";
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    default:
+        cout << "Looking forward to the weekend";  // If there is no any case match then it will run 
+        break;
+    }
+}
