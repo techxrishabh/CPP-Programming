30b0ee35048434d166c91057341247396b63c9af
techxrishabh <rishabh22204901@gmail.com>
2024-02-24 19:28:18 +0530

    add typecasting

---
+++ 9.1. C++ TYPECASTING/typecasting.cpp
@@ -0,0 +1,14 @@
+// TYPECASTING 
+// When we have to interchange something from one data type to another then we use typecasting
+// int, bool, char, float ==> typecasting
+
+
+// lets try to convert float data-type to integer data-type
+
+#include <iostream>
+using namespace std;
+int main(){
+    float x = 7.1;
+    x = int(x); // z  here we convert x to an integer
+    cout << x << "\n";
+}
